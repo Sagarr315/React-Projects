165a46be25609b2343afc6ec92dbefd33ce4c83c
Sagarr315 <sagarrbhor715@gmail.com>
2025-08-01 13:56:53 +0530

    Create Footer.jsx

---
+++ To-Do-List/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+const Footer = () => (
+  <div className="footer-css text-center bg-pink">
+    <p className="display-6 fw-bold">@All Rights Are Reserved</p>
+    <p className="fs-4 fw-semibold">Developed by Sagar Bhor</p>
+  </div>
+);
+
+export default Footer;
