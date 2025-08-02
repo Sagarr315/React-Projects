eb77c9eb3a126f4b3e867f22b1573dd9661daf02
Sagarr315 <sagarrbhor715@gmail.com>
2025-08-02 11:25:21 +0530

    Update App.jsx

--- To-Do-List/src/App.jsx
+++ To-Do-List/src/App.jsx
@@ -3,13 +3,23 @@ import Header from "./components/header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import StudentForm from "./components/studentForm";
+import TaskData from "./components/TaskData";
+import TaskList from "./components/TaskList";
+import { useState } from "react";
 
 function App() {
+  const [tasks,setTasks]=useState(TaskData);
+
+   const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <>
       <Header />
-      <StudentForm/>
-      <Footer/>
+      <StudentForm onAddTask={handleAddTask}/>
+      <TaskList tasks={tasks} />
+      <Footer />
     </>
   );
 }
