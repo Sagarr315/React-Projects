db35bd9200efcce21784046967d6d960647d34ff
Sagar Bhor <sagarrbhor715@gmail.com>
2025-08-03 13:32:59 +0530

    Update App.jsx

--- To-Do-List/src/App.jsx
+++ To-Do-List/src/App.jsx
@@ -5,20 +5,44 @@ import "./index.css";
 import StudentForm from "./components/studentForm";
 import TaskData from "./components/TaskData";
 import TaskList from "./components/TaskList";
-import { useState } from "react";
+import { useState ,useEffect } from "react";
 
 function App() {
-  const [tasks,setTasks]=useState(TaskData);
+  const [tasks, setTasks] = useState(()=>{
+    const storedTasks = localStorage.getItem("tasks");
+  return storedTasks ? JSON.parse(storedTasks) : TaskData;
+  });
+  
+  const [filterType, setFilterType] = useState("");
 
-   const handleAddTask = (newTask) => {
+  const handleAddTask = (newTask) => {
     setTasks([...tasks, newTask]);
   };
 
+  const handleFilterChange = (e) => {
+    setFilterType(e.target.value); // "pending" or "complete"
+  };
+
+  const handleStatusToggle = (id) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, status: "Complete" } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+   useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const filteredTasks =
+    filterType === ""
+      ? tasks
+      : tasks.filter((task) => task.status === filterType);
   return (
     <>
       <Header />
-      <StudentForm onAddTask={handleAddTask}/>
-      <TaskList tasks={tasks} />
+      <StudentForm onAddTask={handleAddTask} />
+      <TaskList tasks={filteredTasks} onFilterChange={handleFilterChange} onStatusToggle={handleStatusToggle} />
       <Footer />
     </>
   );
